fa7bad65968f09714b9bfb3bdf01c92597887bbb
sunyuw <wangsunyu@furuizhonghe.com>
2017-01-06 18:41:19 +0800

    创蓝短信配置example

---
+++ config.example.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'account' => 'abc',
+    'pwd' => 'xxx',
+    'url' => 'http://222.73.117.156/msg/HttpBatchSendSM',
+    'api_balance_query_url' => 'http://sms.253.com/msg/balance',
+];
