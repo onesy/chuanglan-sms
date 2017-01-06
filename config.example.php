9c2c3b292f15e8fdbfcdcccfeeeed80a20cf23d2
sunyuw <wangsunyu@furuizhonghe.com>
2017-01-06 18:43:10 +0800

    完善配置shuoming

--- config.example.php
+++ config.example.php
@@ -1,7 +1,11 @@
 <?php
+/**
+ * 请根据不同的版本以及账户修改配置文件，如果有sdk的版本更新，请通知作者以便及时update代码
+ * @author sunyuw leentingone@gmail.com
+ */
 return [
     'account' => 'abc',
     'pwd' => 'xxx',
     'url' => 'http://222.73.117.156/msg/HttpBatchSendSM',
-    'api_balance_query_url' => 'http://sms.253.com/msg/balance',
+    'api_balance_query_url' => 'http://sms.253.com/msg/balance'
 ];
